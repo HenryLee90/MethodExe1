311e1b71bf8d37c10af020f03f6dbe3f5a9c8e32
Henry Lee <hlee09151@gmail.com>
2022-04-22 13:22:10 -0500

    init

---
+++ MethodExrcise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MethodExrcise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter A Fruit");
+            string fruit =  Console.ReadLine();
+            Console.WriteLine("Enter A Place");
+            string place = Console.ReadLine();
+            Console.WriteLine("Enter A Color");
+            string color = Console.ReadLine();
+            Console.WriteLine("Enter A Creature");
+            string animal  = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+                
+           
+
+
+
+
+
+            Console.WriteLine($"Who lives in a {fruit} under the {place}?");
+            Console.WriteLine($"JohnWard CrabPants");
+            Console.WriteLine($"Absorbent and {color} and porous is he");
+            Console.WriteLine($"JohnWard CrabPants");
+            Console.WriteLine($"If learning csharp be something you wish");
+            Console.WriteLine($"JohnWard CrabPants");
+            Console.WriteLine($"Then drop on the deck and flop like a {animal}!");
+            Console.WriteLine($"JohnWard CrabPants");
+
+
+
+
+
+        }
+    }
+}
